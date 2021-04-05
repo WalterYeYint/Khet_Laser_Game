b05e50799370ac709ac6b26e05aed5d6ca31665a
Kan Yu <WalterYeYint@gmail.com>
2021-04-05 17:44:30 +0630

    Created a homepage.dart and modified main.dart.

---
+++ Khet_Laser_Game/lib/homepage.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[800],
+      body: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: Container(
+              color: Colors.blue,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Colors.red,
+            ),
+          ),
+        ]
+      ),
+    );
+  }
+}
