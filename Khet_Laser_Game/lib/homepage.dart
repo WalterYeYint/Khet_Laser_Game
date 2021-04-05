974c2513e98c0a21ad29465a148639f6d803e7d5
Kan Yu <WalterYeYint@gmail.com>
2021-04-05 17:59:58 +0630

    Created a 8x10 board in homepage.dart.

--- Khet_Laser_Game/lib/homepage.dart
+++ Khet_Laser_Game/lib/homepage.dart
@@ -6,25 +6,38 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int numberOfSquares = 80;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[800],
-      body: Column(
-        children: [
-          Expanded(
-            flex: 3,
-            child: Container(
-              color: Colors.blue,
-            ),
+      body: Column(children: [
+        Expanded(
+          flex: 30,
+          child: Container(
+              child: GridView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: numberOfSquares,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 8),
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(2.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(6),
+                          child: Container(
+                        color: Colors.grey,
+                      )),
+                    );
+                  })),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.red,
           ),
-          Expanded(
-            child: Container(
-              color: Colors.red,
-            ),
-          ),
-        ]
-      ),
+        ),
+      ]),
     );
   }
 }
