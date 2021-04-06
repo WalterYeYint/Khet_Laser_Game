95688aac81a80752a831086862bea14175f420ed
Kan Yu <WalterYeYint@gmail.com>
2021-04-06 13:38:21 +0630

    Created ChessEngine.py & ChessMain.py. Moved images out of sub-images folder.

---
+++ ChessMain.py
@@ -0,0 +1,12 @@
+import pygame as p
+from Chess import ChessEngine
+
+WIDTH = HEIGHT = 512
+DIMENSION_X = 10
+DIMENSION_Y = 8
+SQ_SIZE = HEIGHT // DIMENSION_X
+MAX_FPS = 15
+IMAGES = {}
+
+###################################
+# LOAD THE IMAGES
